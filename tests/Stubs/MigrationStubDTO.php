62f9aab41759141e8fb02e4876ac48873413312e
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2025-06-20 16:01:30 +0300

    refactor: Code style

--- tests/Stubs/MigrationStubDTO.php
+++ tests/Stubs/MigrationStubDTO.php
@@ -10,6 +10,6 @@ class MigrationStubDTO extends MigrationDataTransferObject
 		public string $name,
 		public readonly StubDTO $dto,
 		public readonly TestEnum $enum,
-		public string|null $withDefault = null,
+		public ?string $withDefault = null,
 	) {}
 }
